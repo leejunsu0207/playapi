d595091b3821372a8279c7075e9530bfb8bd50f1
leejunsu <son910220@gmail.com>
2021-03-31 22:52:51 +0900

    first commit

---
+++ src/main/java/com/example/playapi/PlayapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.playapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/playapi/PlayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
